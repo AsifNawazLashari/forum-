3e27690980eed1676b2fe5f64dae73f201fd8d7e
AsifNawazLashari <asiflashari143@gmail.com>
2024-04-17 14:00:47 +0500

    Create firebase_config.php

---
+++ firebase_config.php
@@ -0,0 +1,12 @@
+<?php
+// Firebase configuration details
+$firebaseConfig = array(
+    'apiKey' => 'AIzaSyDZ4IjtvPGypB84evukROyNHwBvkhGyBkM',
+    'authDomain' => 'alif-ac574.firebaseapp.com',
+    'databaseURL' => 'https://alif-ac574-default-rtdb.firebaseio.com',
+    'projectId' => 'alif-ac574',
+    'storageBucket' => 'alif-ac574.appspot.com',
+    'messagingSenderId' => '261987716644',
+    'appId' => '1:261987716644:web:02d0beeee8a04f998a12b1'
+);
+
